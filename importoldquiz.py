ba2bfd1dd910d9a02e35a1b5f2b3e40d249e9927
Vitaliy Filippov <vitalif@yourcmc.ru>
2010-08-25 16:37:10 +0000

    Bug 61623 - Python script to convert old quizzes to new Wiki markup

---
+++ importoldquiz.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+
+def printquestions(questions):
+  for q in questions:
+    sys.stdout.write("== Вопрос")
+    if 'label' in q and q['label'].strip() != '':
+      sys.stdout.write(": "+q['label'].strip())
+    if 'tags' in q:
+      sys.stdout.write(', ' + ', '.join(q['tags']))
+    sys.stdout.write(" ==\n\n"+q['question'].strip()+"\n\n")
+    m = 0
+    for c in q['choices']:
+      if c[1].find('\n') != -1:
+        m = 1
+    if m:
+      for c in q['choices']:
+        if c[0]:
+          sys.stdout.write("=== Правильный ответ ===\n\n")
+        else:
+          sys.stdout.write("=== Ответ ===\n\n")
+        sys.stdout.write(c[1].strip()+"\n\n")
+    else:
+      sys.stdout.write("=== Ответы ===\n\n")
+      for c in q['choices']:
+        if c[0]:
+          sys.stdout.write("* Правильный ответ: ")
+        else:
+          sys.stdout.write("* ")
+        sys.stdout.write(c[1]+"\n")
+      sys.stdout.write("\n")
